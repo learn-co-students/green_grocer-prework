3340f0a909f45b6b1696c0cd42f28c4fe3e24661
otherworldlyguardian <otherworldlyguardian@gmail.com>
2017-04-02 16:27:23 -0400

    Let's go shopping.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,58 @@
 def consolidate_cart(cart)
-  # code here
+  return_hash = {}
+  if cart.length == 1
+    cart[0].each do |item, info|
+      info[:count] = 1
+      return_hash[item] = info
+    end
+  end
+  cart.uniq do |item_info|
+    item_info.each do |item, info|
+      info[:count] = cart.count { |hash| hash.keys[0] == item }
+      return_hash[item] = info
+    end
+  end
+  return_hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  if coupons.empty?
+    return cart
+  end
+  return_hash = {}
+  cart.each do |item, info|
+    coupons.each do |discount|
+      if discount[:item] == item && discount[:num] <= info[:count]
+        coupon_count = info[:count] / discount[:num]
+        info[:count] = info[:count] - (discount[:num] * coupon_count)
+        return_hash[item] = info
+        return_hash[item + " W/COUPON"] = {:price => discount[:cost], :clearance => info[:clearance], :count => coupon_count}
+      else
+        return_hash[item] = info
+      end
+    end
+  end
+  return_hash
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |item, info|
+    if info[:clearance]
+      info[:price] = (info[:price] * 0.8).round(2)
+    end
+  end
 end
 
 def checkout(cart, coupons)
-  # code here
+  c_cart = consolidate_cart(cart)
+  discount_cart = apply_coupons(c_cart, coupons)
+  clear_cart = apply_clearance(discount_cart)
+  total = 0
+  clear_cart.each do |item, info|
+    total += (info[:count] * info[:price]).round(2)
+  end
+  if total > 100
+    total = (total * 0.9).round(2)
+  end
+  total
 end
