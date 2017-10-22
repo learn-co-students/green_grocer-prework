0e74b204718a5fbf88b8f8e52b91cbcb4fae4c38
JoshD1 <joshdeiner75@gmail.com>
2017-10-22 14:11:16 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,11 +1,11 @@
 def consolidate_cart(cart)
   cart.each_with_object({}) do |item, result|
-    item.each do |type, attributes|
+    item.each do |type, attributes| #enters loop
       if result[type]
-        attributes[:count] += 1
+        attributes[:count] += 1 # creates new variable and addes to it
       else
         attributes[:count] = 1
-        result[type] = attributes
+        result[type] = attributes # still adds new variable
       end
     end
   end
@@ -14,27 +14,27 @@ end
 def apply_coupons(cart, coupons)
   coupons.each do |coupon|
     name = coupon[:item]
-    if cart[name] && cart[name][:count] >= coupon[:num]
-      if cart["#{name} W/COUPON"]
-        cart["#{name} W/COUPON"][:count] += 1
+    if cart[name] && cart[name][:count] >= coupon[:num] # if true, then applies coupon
+      if cart["#{name} W/COUPON"] # creates new var that has effect of coupon
+        cart["#{name} W/COUPON"][:count] += 1 #creates and adds count
       else
-        cart["#{name} W/COUPON"] = {:count => 1, :price => coupon[:cost]}
+        cart["#{name} W/COUPON"] = {:count => 1, :price => coupon[:cost]} # creates coupon but unused
         cart["#{name} W/COUPON"][:clearance] = cart[name][:clearance]
       end
-      cart[name][:count] -= coupon[:num]
+      cart[name][:count] -= coupon[:num] # regardless of above, eh, applies effect of the coupon if any.
     end
   end
-  cart
+  cart # returns final version with coupon applied
 end
 
 def apply_clearance(cart)
   cart.each do |name, properties|
-    if properties[:clearance]
+    if properties[:clearance] # checks for clearance
       updated_price = properties[:price] * 0.80
-      properties[:price] = updated_price.round(2)
+      properties[:price] = updated_price.round(2) #adding new price value in place
     end
   end
-  cart
+  cart # return new cart
 end
 
 def checkout(cart, coupons)
