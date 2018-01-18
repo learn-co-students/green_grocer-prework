bb05c6d04e89351e708e923807543d2014b106eb
Caroline Lee <carolinel2610@gmail.com>
2018-01-18 23:52:42 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,31 @@
+require "pry"
 def consolidate_cart(cart)
-  # code here
+  hash = {}
+  cart.each{|element|
+    element.each{ |prod, p_hash|
+      # binding.pry
+      if hash[prod] == nil
+        hash[prod] = p_hash
+        hash[prod][:count] = 1
+      else
+        hash[prod][:count] += 1
+      end
+    }
+  }
+hash
 end
 
 def apply_coupons(cart, coupons)
   # code here
+  cart.each{ |prod, p_hash|
+    binding.pry
+    if prod == coupons[:item] && p_hash[:count] >= coupons[:num]
+      p_hash[:count] = p_hash[:count] % coupons[:num]
+      new_prod = prod + " W/COUPON"
+      cart[new_prod] = {price: coupons[:cost], clearance: p_hash[:clearance], count: p_hash[:count] / coupons[:num]}
+    end
+  }
+  cart
 end
 
 def apply_clearance(cart)
