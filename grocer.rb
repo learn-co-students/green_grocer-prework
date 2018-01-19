7244b5315c207b70feaff23720e7832ebb4255ce
Caroline Lee <carolinel2610@gmail.com>
2018-01-19 03:09:57 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -12,37 +12,58 @@ def consolidate_cart(cart)
       end
     }
   }
-hash
+  hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  # cart = {"AVOCADO"=>{:price=>3.0, :clearance=>true, :count=>5}}
+  # coupons = [{:item=>"AVOCADO", :num=>2, :cost=>5.0},{:item=>"AVOCADO", :num=>2, :cost=>5.0}]
   array = []
   cart.each{ |prod, p_hash|
     coupons.each {|coupon|
-      # binding.pry
       if prod == coupon[:item] && p_hash[:count] >= coupon[:num]
         array << prod + " W/COUPON"
         array << coupon[:cost]
         array << p_hash[:clearance]
-        array << p_hash[:count] / coupon[:num]
-        p_hash[:count] = p_hash[:count] % coupon[:num]
-        # new_prod = prod + " W/COUPON"
-        # cart[new_prod] = {price: coupon[:cost], clearance: p_hash[:clearance], count: p_hash[:count] / coupon[:num]}
+        array << 1 #p_hash[:count] / coupon[:num]
+        p_hash[:count] = p_hash[:count] - coupon[:num] # - used to be %
       end
     }
   }
+  # binding.pry
   until array == []
-    cart[array[0]] = {price: array[1], clearance: array[2], count: array[3]}
+    if cart[array[0]] == nil
+      cart[array[0]] = {price: array[1], clearance: array[2], count: array[3]}
+    else
+      # cart[array[0]][:price] = cart[array[0]][:price] + array[1]
+      cart[array[0]][:count] = cart[array[0]][:count] + array[3]
+    end
     array.shift(4)
   end
   cart
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each{ |prod, p_hash|
+    # binding.pry
+    if p_hash[:clearance]
+      p_hash[:price]= (p_hash[:price] * 0.8).round(2)
+    end
+  }
+  cart
 end
 
 def checkout(cart, coupons)
-  # code here
+  cart = consolidate_cart(cart)
+  cart = apply_coupons(cart, coupons)
+  cart = apply_clearance(cart)
+  # cart = consolidate_cart(cart)
+  total = 0
+  cart.each { |prod, p_hash|
+    total += p_hash[:price]
+  }
+  # if total > 100
+  #   total = (total*0.9).round(2)
+  # end
+  total
 end
