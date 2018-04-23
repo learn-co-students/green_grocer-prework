8de18e6eec8d21a4509c1d6ba90604ec0a55a059
github username <github email address>
2018-04-23 12:27:41 -0700

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,70 @@
 def consolidate_cart(cart)
-  # code here
+  new_cart = {}
+
+  item_list = []
+  cart.each do |item|
+    item.each do |key, value|
+      item_list << key
+    end
+  end
+
+  cart.each do |item_hash|
+    item_hash.each do |item_name, item_info|
+      if new_cart[item_name].nil?
+        new_cart[item_name] = item_info.merge(:count => item_list.count(item_name))
+      end
+    end
+  end
+  new_cart
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupon_count = {}
+  coupons.each do |coupon|
+    item = coupon[:item]
+    if coupon_count[item].nil?
+      coupon_count[item] = 1
+    else
+      coupon_count[item] += 1
+    end
+
+    if cart.keys.include?(item)
+      if cart[item][:count] >= coupon[:num]
+        cart[item][:count] -= coupon[:num]
+        cart["#{item} W/COUPON"] = {:price => coupon[:cost], :clearance => cart[item][:clearance], :count => coupon_count[item]}
+      end
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |item, info|
+    if cart[item][:clearance] == true
+      cart[item][:price] -= cart[item][:price]* 0.2
+    end
+  end
+  cart
 end
 
 def checkout(cart, coupons)
-  # code here
+  final_cart = consolidate_cart(cart)
+  apply_coupons(final_cart, coupons)
+  apply_clearance(final_cart)
+
+  final_cart.each do |item, info|
+    if final_cart[item][:count] == 0
+      final_cart.delete(item)
+    end
+  end
+
+  total = 0
+  final_cart.each do |item, info|
+    total += final_cart[item][:price] * final_cart[item][:count]
+  end
+
+  if total > 100
+    total -= total * 0.1
+  end
+  total
 end
