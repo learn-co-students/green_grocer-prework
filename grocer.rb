7158deabc4af9ea35faf075bf4cbb8388ae8f7f4
jarretbryan <jarret.bryan@gmail.com>
2018-05-03 15:38:04 -0400

    to refactor

--- grocer.rb
+++ grocer.rb
@@ -18,21 +18,33 @@ def consolidate_cart(cart)
   consol_cart
 end #method
 
-=begin
+
 def apply_coupons(cart, coupons)
-  # code here
-  #binding.pry
-  coupon_cart = {}
-  cart.each do |item|
-    item.each do |name,stats|
-      binding.pry
-      if coupons[0][:item] == name
-        puts name
-      end #conditonal statement
-    end #item iteration
-  end #cart iteration
+  #coupon_cart = {}
+  coupon_cart = cart
+  if coupons.length == 0
+    coupon_cart
+  end
+
+  coupons.each do |coupon|
+    name = coupon[:item]
+    if coupon_cart.has_key?(name) && coupon_cart[name][:count] >= coupon[:num]
+      coupon_cart[name][:count] -= coupon[:num]
+      new_key = "#{name} W/COUPON"
+      if coupon_cart.has_key?(new_key)
+        coupon_cart[new_key][:count] += 1
+      else
+        coupon_cart[new_key] = {
+          :price => coupon[:cost],
+          :clearance => coupon_cart[name][:clearance],
+          :count => 1
+        }
+      end #second conditional
+    end #first coupon_cart condional
+  end #coupons.each iteration
+  coupon_cart
 end #method
-=end
+
 
 def apply_clearance(cart)
   # code here
@@ -40,7 +52,7 @@ def apply_clearance(cart)
   clear_cart.each do |item, attributes|
 
 
-      binding.pry
+      #binding.pry
       if attributes[:clearance] == true
         attributes[:price] = (attributes[:price].to_f * (0.8)).round(2)
       else
@@ -49,10 +61,24 @@ def apply_clearance(cart)
 
 
   end
-  binding.pry
+  #binding.pry
   clear_cart
 end
 
 def checkout(cart, coupons)
   # code here
+  consol_cart = consolidate_cart(cart)
+  coupon_cart = apply_coupons(consol_cart, coupons)
+  clear_cart = apply_clearance(coupon_cart)
+
+  total = 0
+
+  clear_cart.each do |item, attributes|
+    total += attributes[:price] * attributes[:count]
+  end
+
+  if total > 100
+    total = (total * (0.9)).round(2)
+  end
+total
 end
