557f8f9eb61e7d1409cea0a18e2ada9f40fd36be
github username <github email address>
2018-05-31 01:56:10 -0400

    First Attempt

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,58 @@
 def consolidate_cart(cart)
-  # code here
+  return_hash = {}
+  cart.each do |item|
+    item.each do |food, properties|
+      i = 1
+      if return_hash[food] == nil
+        return_hash[food] = properties.merge!(:count => i)
+      elsif return_hash[food].has_key?(:count)
+        return_hash[food][:count] += 1
+      end
+    end
+  end
+  return_hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  return_hash = {}
+  cart.each do |food, properties|
+    coupons.each do |coupon|
+      if coupon[:item] == food && coupon[:num] <= properties[:count]
+        #properties[:count] = properties[:count] - coupon[:num]
+        return_hash["#{food} W/COUPON"] = {:price => coupon[:cost], :clearance => properties[:clearance], :count => 1}
+        return_hash[food] = {:price => properties[:price], :clearance => properties[:clearance], :count => (properties[:count] - coupon[:num])}
+      else
+        return_hash[food] = properties
+      end
+    end
+  end
+  return_hash
 end
 
 def apply_clearance(cart)
-  # code here
+  return_hash = {}
+  cart.each do |food, properties|
+    if properties[:clearance] == true
+      return_hash[food] = {:price => (properties[:price] * 0.80).round(2), :clearance => properties[:clearance], :count => properties[:count]}
+    else
+      return_hash[food] = properties
+    end
+  end
+  return_hash
 end
 
 def checkout(cart, coupons)
-  # code here
+  consolidate_cart(cart)
+  apply_coupons(cart, coupons)
+  apply_clearance(cart)
+
+  cart_total = 0.00
+  cart.each do |food, properties|
+    cart_total += properties[:price]
+  end
+
+  if cart_total > 100.00
+    cart_total = cart_total * 0.90
+
+  cart_total
 end
