324a01456ed49dad9278d54029cc2c6720b9f56e
Stephen F <stephen@farver.io>
2018-06-05 18:40:34 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,34 @@
 def consolidate_cart(cart)
-  # code here
+  output_hash = {}
+  
+  cart.each do |el|
+    el.each do |name, values|
+      if !output_hash[name]
+        output_hash[name] = values
+        output_hash[name].merge!(count: 1)
+      else
+        output_hash[name][:count] += 1
+      end
+    end
+  end
+  output_hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  cart.map do |cart_item, details|
+    puts coupons
+    puts "NAMEEEE: #{coupons["num".to_sym]} Extra text"
+    if coupons[:item] == cart_item
+      coupon_count = coupons[:num] > cart_item[:count] ? cart_item[:count] : coupons[:num]
+      cart["#{cart_item} W/COUPON"] = {
+        price: coupons[:cost],
+        clearance: true,
+        count: coupon_count
+      }
+      
+      cart_item[:count] -= coupon_count
+    end
+  end
 end
 
 def apply_clearance(cart)
