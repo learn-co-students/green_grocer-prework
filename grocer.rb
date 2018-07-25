abfdc32783acb30556dd9e59b551c3a1bd2927c7
Olivia Tian <oliviatian27@gmail.com>
2018-07-25 20:50:08 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,55 @@
 def consolidate_cart(cart)
-  # code here
+  result={}
+  cart.each do |item|
+    item.each do |key,value|
+       result[key]||= value
+       result[key][:count]||=0
+       result[key][:count]+=1
+    end
+  end
+  result
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+   coupons.each do |coupon|
+     fruit=coupon[:item]
+     name=fruit+" W/COUPON"
+     if cart.has_key?(fruit)
+         if cart[fruit][:count]>=coupon[:num]
+            cart[fruit][:count]-=coupon[:num]
+            if cart[name].nil?
+                cart[name]={}
+                cart[name][:price]=coupon[:cost]
+                cart[name][:clearance]=cart[coupon[:item]][:clearance]
+                cart[name][:count]=1
+            else
+                cart[name][:count]+=1
+            end
+          end
+     end
+   end
+  cart
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do|item,detail|
+   if detail[:clearance]==true
+     detail[:price]=(detail[:price]*0.8).round(2)
+   end
+  end
+  cart
 end
 
 def checkout(cart, coupons)
-  # code here
+  cart=consolidate_cart(cart)
+  cart=apply_coupons(cart,coupons)
+  cart=apply_clearance(cart)
+  total=0
+     cart.each do |item,detail|
+       total+=(detail[:price]*detail[:count])
+     end
+     if total>100
+        total=(total*0.9).round(2)
+     end
+   total  
 end
