d91f0e9a1f086c7105ad5bb25bef3d924ea44222
Ciaran Morinan <cmorinan@gmail.com>
2018-08-05 14:16:32 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,54 @@
 def consolidate_cart(cart)
-  # code here
+  cart.each_with_object({}) do |item, hash|
+    item.each do |product, attributes|
+      hash[product] = attributes
+      hash[product][:count] ||= 0
+      hash[product][:count] += 1
+    end
+  end
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |coupon|
+    coup_item = coupon[:item]
+    break if !cart[coup_item]
+    usable_coupons = cart[coup_item][:count] / coupon[:num]
+    if usable_coupons > 0
+      cart[coup_item][:count] -= usable_coupons * coupon[:num]
+      cart["#{coup_item} W/COUPON"] = 
+        {
+          :price => coupon[:cost],
+          :clearance => cart[coup_item][:clearance],
+          :count => usable_coupons
+        }
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |product, attributes|
+    if attributes[:clearance] == true
+      attributes[:price] = (attributes[:price] *= 0.80).round(2)
+    end
+  end
 end
 
+def total_cart(cart)
+  total = 0
+  cart.each do |product, attributes|
+    total += attributes[:price] * attributes[:count]
+  end
+  total
+end
+  
 def checkout(cart, coupons)
-  # code here
+  cart = consolidate_cart(cart)
+  cart = apply_coupons(cart, coupons)
+  cart = apply_clearance(cart)
+  cart = total_cart(cart)
+  if cart > 100
+    cart *= 0.90
+  end
+  cart
 end
