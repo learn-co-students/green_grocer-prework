464283ade0f75dec8f99dae999f8727feadc11db
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-20 22:44:54 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,48 +1,20 @@
-require "pry"
-# each_with_object
 def consolidate_cart(cart)
-  cart_hash = {}
-  cart.each do |food_item_hash|
-    food_item_frequency = cart.count(food_item_hash)
-    food_item_hash.each do |food_item_key, attribute_hash|
-      attribute_hash[:count] = food_item_frequency
-      
-# attribute_hash.each do |attribute_symbol, attribute_value|
-        if cart_hash = {}
-          cart_hash = {food_item_key => attribute_hash}
-        else  cart_hash[food_item_key] = attribute_hash
-        end 
-          #  binding.pry #  After 3 exits:   
-              #  food_item_key   properly returns    "AVOCADO"  ;  
-              #   attribute_hash properly returns  {:price=>3.0, :clearance=>true, :count=>2}
-              #  {food_item_key => attribute_hash} properly returns  {"AVOCADO"=>{:price=>3.0, :clearance=>true, :count=>2}}  ;    
-              #    cart_hash[food_item_key]   properly returns   {:price=>3.0, :clearance=>true, :count=>2}    ;   and  
-              # cart_hash properly returns {"AVOCADO"=>{:price=>3.0, :clearance=>true, :count=>2} .
-  # cart_hash = cart_hash.uniq
-              #     cart_hash = {food_item_key => {attribute_symbol => attribute_value, 
-  # attribute_symbol => attribute_value, :count => food_item_frequency}}
-    end 
-          #  binding.pry # After 3 exits, cart_hash IMproperly returns merely 
-              # {"AVOCADO"=>{:price=>3.0, :clearance=>true, :count=>2} . 
-              # At this point,  cart_hash  should be returning a hash of 3 key/value pairs (including dupes, depending on the     cart     ).
+  consolidated_cart = {}
+  cart.each do |item|
+    item.each do |item_key, item|
+      item_count = count(cart, item_key)
+      consolidated_cart[item_key] = {count: item_count, price: item[:price], clearance: item[:clearance]}
+    end
   end 
-  cart_hash
-          #  binding.pry   # After 3 exits, cart_hash IMproperly returns merely 
-              #{"AVOCADO"=>{:price=>3.0, :clearance=>true, :count=>2} . 
-              # At this point,  cart_hash  should be returning a hash of 3 key/value pairs (including dupes, depending on the     cart     ).
+  consolidated_cart
 end
 
-
-def apply_coupons(cart, coupons)
-  
-end
-
-
-def apply_clearance(cart)
-  
-end
-
-
-def checkout(cart, coupons)
-  
+def count(cart, key)
+  count = 0
+  cart.each do |item|
+    item.each do  |item_key, item|
+     count +=1 if item_key == key
+    end
+  end
+  count
 end
