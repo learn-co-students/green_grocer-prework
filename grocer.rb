9e556b5a6e93fce1304ee456017e5cd020890d72
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-20 19:34:25 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,37 +1,27 @@
 require "pry"
 
 def consolidate_cart(cart)
-  #cart_hash = {}
-  cart.each_with_object({}) do |food_item_key, food_item_value|
- #   food_item_frequency = cart.count(food_item_hash)
- #   food_item_hash.each do |food_item_key, attribute_hash|
- #     attribute_hash[:count] = food_item_frequency
-#      binding.pry # after exiting three times through the first cart in the tests, food_item_hash properly returns {"AVOCADO"=>{:price=>3.0, :clearance=>true, :count=>2}} .  The count properly registers as 2   (and, of course, attribute_hash properly returns {:price=>3.0, :clearance=>true, :count=>2}   ).
+  cart_hash = {}
+  cart.each do |food_item_hash|
+    food_item_frequency = cart.count(food_item_hash)
+    food_item_hash.each do |food_item_key, attribute_hash|
+      attribute_hash[:count] = food_item_frequency
+ #     binding.pry # after exiting three times through the first cart in the tests, food_item_hash properly returns {"AVOCADO"=>{:price=>3.0, :clearance=>true, :count=>2}} .  The count properly registers as 2   (and, of course, attribute_hash properly returns {:price=>3.0, :clearance=>true, :count=>2}   ).
 # attribute_hash.each do |attribute_symbol, attribute_value|
-
-  food_item_key.each do |k, v|
- #   binding.pry
-        if food_item_key[:k] 
-          v[:count] += 1
-            
-          
-        else  
-          binding.pry
-          v[:count] = 1
-          
-          food_item_key[food_item_value] = v
+        if cart_hash = {}
+          cart_hash = {food_item_key => attribute_hash}
+        else  cart_hash[food_item_key] = attribute_hash
         end 
-      
-  #      cart_hash = cart_hash.uniq
+ #       cart_hash = cart_hash.uniq
 #     cart_hash = {food_item_key => {attribute_symbol => attribute_value, attribute_symbol => attribute_value, :count => food_item_frequency}}
     end 
   end 
- # cart_hash
+  cart_hash
 end
 
 
 def apply_coupons(cart, coupons)
-   
+  
 end
 
 
