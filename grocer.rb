ca52ca783f23639feb88925b3ff59ee399bd532c
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-20 14:20:48 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,20 @@
+require "pry"
+
 def consolidate_cart(cart)
-  
+  cart_hash = {}
+  new_cart_array = []
+  cart.each do |food_item_hash|
+    
+    food_item_hash.each do |food_item_key, attribute_hash|
+      attribute_hash.each do |attribute_symbol, attribute_value|
+        food_item_frequency = cart.count(food_item_hash)
+  binding.pry    
+   #     cart_hash = {food_item_key => {attribute_symbol => attribute_value, attribute_symbol => attribute_value, :count => food_item_frequency}}
+      end
+    end 
+  end 
+  cart_hash
+
 end
 
 
