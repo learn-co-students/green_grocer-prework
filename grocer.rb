463750b3dc5fc7031b17d3a6586865bcf71c5dbb
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-20 20:16:37 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -18,7 +18,7 @@ def consolidate_cart(cart)
               #  {food_item_key => attribute_hash} properly returns  {"AVOCADO"=>{:price=>3.0, :clearance=>true, :count=>2}}  ;    
               #    cart_hash[food_item_key]   properly returns   {:price=>3.0, :clearance=>true, :count=>2}    ;   and  
               # cart_hash properly returns {"AVOCADO"=>{:price=>3.0, :clearance=>true, :count=>2} .
- #       cart_hash = cart_hash.uniq
+#       cart_hash = cart_hash.uniq
 #     cart_hash = {food_item_key => {attribute_symbol => attribute_value, attribute_symbol => attribute_value, :count => food_item_frequency}}
     end 
   end 
