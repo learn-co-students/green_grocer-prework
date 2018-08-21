22fb041e7796ba0d2bc28cf1348654d4ba1df6b4
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-21 00:03:26 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,21 +1,19 @@
 require "pry"
 # each_with_object
+
 def consolidate_cart(cart)
   cart_hash = {}
   cart.each do |food_item_hash|
     food_item_frequency = cart.count(food_item_hash)
     food_item_hash.each do |food_item_key, attribute_hash|
       attribute_hash[:count] = food_item_frequency
-# attribute_hash.each do |attribute_symbol, attribute_value|
         if cart_hash == {}
           cart_hash = {food_item_key => attribute_hash}
         else  cart_hash[food_item_key] = attribute_hash
-        end 
-          #  binding.pry #  After 3 exits:  # cart_hash properly returns {"AVOCADO"=>{:price=>3.0, :clearance=>true, :count=>2} .
+        end
 # cart_hash = cart_hash.uniq
 # cart_hash = {food_item_key => {attribute_symbol => attribute_value, attribute_symbol =>       attribute_value, :count => food_item_frequency}}
     end 
-          #  binding.pry # After 3 exits, cart_hash IMproperly returns merely the same as last binding.pry above: {"AVOCADO"=>{:price=>3.0, :clearance=>true, :count=>2} .  At this point,  cart_hash  should be returning a hash of 3 key/value pairs (including dupes, depending on the     cart     ), each value of which should be a hash like "AVOCADO"'s. 
   end 
   cart_hash
           #  binding.pry # Diddo last binding.pry above.
