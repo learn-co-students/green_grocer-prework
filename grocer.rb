d7f412657fd39b815a0f0e6e9d6d9eecc2066916
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-21 16:21:56 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -24,9 +24,9 @@ def apply_coupons(cart, coupons)
   final_hash = nil 
   coupons.each do |coupon_hash|
     if cart.keys.include?(coupon_hash[:item]) 
-      counter = coupons.count(coupon_hash[:item])
+#      counter = coupons.count(coupon_hash[:item])
       cart[coupon_hash[:item]][:count] = cart[coupon_hash[:item]][:count] - coupon_hash[:num]
-       cart[coupon_hash[:item] + " W/COUPON"] = {:price => coupon_hash[:cost], :clearance => cart[coupon_hash[:item]][:clearance], :count => counter}
+       cart[coupon_hash[:item] + " W/COUPON"] = {:price => coupon_hash[:cost], :clearance => cart[coupon_hash[:item]][:clearance], :count => 1}
     end 
   end 
   final_hash = cart 
