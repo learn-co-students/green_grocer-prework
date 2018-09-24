c562d5995f1d527a4034502ac48015f76c2472ca
Mirco guidetti <mirco@guidetti.co>
2018-09-24 23:22:29 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -30,12 +30,14 @@ def apply_coupons(cart, coupons)
       
       
       if cart["#{name} W/COUPON"] 
+        
         cart["#{name} W/COUPON"][:count] += 1
       else
         cart["#{name} W/COUPON"] = {:price => coupon[:cost], 
         :clearance => cart[name][:clearance], :count => 1 }
       end
       cart[name][:count] -= coupon[:num]
+      
     end
   end
   cart
