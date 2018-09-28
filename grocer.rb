09dc0d4ed5df8c6b7ba82995ceb6ed706aec3e6d
Malcolm Leake <echelon1089@gmail.com>
2018-09-28 22:51:39 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,11 +1,30 @@
 require 'pry'
 def consolidate_cart(cart)
-  binding.pry
-  # code here
-end
+thanks= {}
+  cart.each do |item|
+    item.each do |fruit, attributes|
+    
+     if thanks[fruit]
+      attributes[:count] += 1
+     else
+      attributes[:count] = 1
+      thanks[fruit] = attributes
+     end
+   end
+ end
+ thanks
+    end
+  
+
+
 
 def apply_coupons(cart, coupons)
   # code here
+coupons.each do |coupon|
+    name = coupon[:item]
+  binding.pry
+ cart["#{name} W/COUPON"] = {:price => coupon[:cost], :clearance => cart[name][:clearance], :count => 1}
+  end
 end
 
 def apply_clearance(cart)
