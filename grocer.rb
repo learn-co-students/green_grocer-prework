ff10da2fc5607ac9bbd595dca06436c21c75b292
github username <crishanks12@gmail.com>
2018-11-19 22:32:55 -0700

    progress on green grocer lab

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,47 @@
+require "pry"
+
+# [
+#   {"AVOCADO" => {:price => 3.0, :clearance => true }},
+#   {"AVOCADO" => {:price => 3.0, :clearance => true }},
+#   {"KALE"    => {:price => 3.0, :clearance => false}}
+# ]
+
 def consolidate_cart(cart)
-  # code here
+  result = {}
+  cart.each do |item|
+    item.each do |key, object_value|
+      if result[key] == nil
+        result[key] = object_value
+        result[key][:count] = 1
+      else
+        result[key][:count] += 1
+      end
+    end
+  end
+  result
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  new_cart = {}
+  cart.each do |item_key, attributes|
+    if new_cart[item_key] == nil
+      new_cart[item_key] = cart[item_key]
+    end
+    coupons.each do |coupon|
+      if attributes[:count] == nil
+        attributes[:count] = 1
+      else
+        attributes[:count] += 1
+      end
+      if item_key == coupon[:item]
+        attributes[:count] -= coupon[:num]
+        new_cart["#{item_key} W/COUPON"] = {:price => coupon[:cost], :clearance => attributes[:clearance], :count => attributes[:count]}
+        new_cart[item_key][:count] -= new_cart["#{item_key} W/COUPON"][:count]
+        # binding.pry
+      end
+    end
+  end
+  new_cart
 end
 
 def apply_clearance(cart)
