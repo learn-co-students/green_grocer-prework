710bdb6ef8f1371658b01ff8db61243aca972e27
github username <crishanks12@gmail.com>
2018-11-20 20:16:33 -0700

    Done.

--- grocer.rb
+++ grocer.rb
@@ -21,33 +21,49 @@ def consolidate_cart(cart)
   result
 end
 
-def apply_coupons(cart, coupons)
-  new_cart = {}
-  cart.each do |item_key, attributes|
-    if new_cart[item_key] == nil
-      new_cart[item_key] = cart[item_key]
-    end
+def apply_coupons(cart_before_coupons, coupons)
+  cart_after_coupons = {}
+  cart_before_coupons.each do |grocery_item_name, grocery_attributes|
+    cart_after_coupons[grocery_item_name] = cart_before_coupons[grocery_item_name]
     coupons.each do |coupon|
-      if attributes[:count] == nil
-        attributes[:count] = 1
-      else
-        attributes[:count] += 1
-      end
-      if item_key == coupon[:item]
-        attributes[:count] -= coupon[:num]
-        new_cart["#{item_key} W/COUPON"] = {:price => coupon[:cost], :clearance => attributes[:clearance], :count => attributes[:count]}
-        new_cart[item_key][:count] -= new_cart["#{item_key} W/COUPON"][:count]
-        # binding.pry
+      if grocery_item_name == coupon[:item]
+        coupon_number = coupon[:num]
+        if grocery_attributes[:count] >= coupon[:num]
+          items_after_coupon = grocery_attributes[:count] - coupon_number
+          grocery_attributes[:count] = items_after_coupon
+        else
+          next
+        end
+        if cart_after_coupons["#{grocery_item_name} W/COUPON"]
+          cart_after_coupons["#{grocery_item_name} W/COUPON"][:count] += 1
+        else
+          cart_after_coupons["#{grocery_item_name} W/COUPON"] = {:price => coupon[:cost], :clearance => grocery_attributes[:clearance], :count => 1}
+        end
       end
     end
   end
-  new_cart
+  cart_after_coupons
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |item, attributes|
+    if attributes[:clearance]
+      attributes[:price] = (attributes[:price] * 0.8).round(2)
+    end
+  end
+  cart
 end
 
 def checkout(cart, coupons)
-  # code here
+  total_cost = 0
+  consolidated_cart = consolidate_cart(cart)
+  consolidated_cart_with_coupons = apply_coupons(consolidated_cart, coupons)
+  apply_clearance(consolidated_cart_with_coupons)
+  consolidated_cart_with_coupons.each do |grocery_item_name, grocery_attribute|
+    total_cost += grocery_attribute[:price] * grocery_attribute[:count]
+  end
+  if total_cost > 100
+    total_cost *= 0.9
+  end
+  total_cost
 end
