cf74735f9424a8eab9e30dac213e3ace32f8ba37
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-21 20:54:36 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -2,12 +2,12 @@ require 'pry'
 def consolidate_cart(cart)
   cart_hash = {}
   cart.each do |element|
-    element.each do |name, parameters|
-        cart_hash[name] = parameters
-        cart_hash[name][:count] ||= 0
-          element.each do |count_name, count_parameters|
-            if name == count_name
-              cart_hash[name][:count] += 1
+    element.each do |item, parameters|
+        cart_hash[item] = parameters
+        cart_hash[item][:count] ||= 0
+          element.each do |count_item, count_parameters|
+            if item == count_item
+              cart_hash[item][:count] += 1
             end
           end
     end
@@ -16,7 +16,25 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  
+  cart_hash = {}
+  cart.each do |item, parameters|
+    parameters.each do |parameter, value|
+      cart_hash[item] = parameters
+      coupons.each do |element|
+        element.each do |coupon_parameter, coupon_value|
+          if element[:item] == item
+            cart_hash["#{item} W/COUPON"] = parameters
+            binding.pry
+            cart_hash["#{item} W/COUPON"][:price] = element[:cost]
+            cart_hash["#{item} W/COUPON"][:count] = 1
+            cart_hash[item][:count] -= element[:num]
+          end
+        end
+      end 
+    end 
+  end
+  #binding.pry
+  cart_hash
 end
 
 def apply_clearance(cart)
