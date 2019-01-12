b9d0770252f011f3032be68609d158b420d1af90
github username <github email address>
2019-01-12 18:35:08 -0500

    Completed Green Grocer Lab

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,59 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
+  cart_hash = {}
+  cart.each do |array|
+    array.each do |item, item_hash|
+        if cart_hash[item] == nil
+          cart_hash[item] = item_hash.merge(:count => 1)
+        else
+          cart_hash[item][:count] += 1
+        end
+    end
+  end
+  cart_hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |coupon|
+  name = coupon[:item]
+    if cart[name] && cart[name][:count] >= coupon[:num]
+      if cart["#{name} W/COUPON"]
+        cart["#{name} W/COUPON"][:count] += 1
+      else
+        cart["#{name} W/COUPON"] = {:count => 1, :price => coupon[:cost], :clearance => cart[name][:clearance]}
+      end
+      cart[name][:count] -= coupon[:num]
+    else
+      cart
+    end
+  end
+cart
 end
 
 def apply_clearance(cart)
-  # code here
+    cart.each do |item, item_hash|
+      if item_hash[:clearance]
+        new_price = (item_hash[:price] * 0.80)
+        item_hash[:price] = new_price.round(2)
+      end
+    end
+cart
 end
 
 def checkout(cart, coupons)
-  # code here
+cart_total = 0
+consolidated_cart = consolidate_cart(cart)
+cart_after_coupon = apply_coupons(consolidated_cart, coupons)
+final_cart = apply_clearance(cart_after_coupon)
+
+  final_cart.each do |item, item_hash|
+    cart_total += item_hash[:price] * item_hash[:count]
+  end
+
+  if cart_total > 100
+    cart_total = (cart_total * 0.90)
+  end
+
+  cart_total
 end
