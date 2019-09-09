79c3b9c0f1608688de5289283cc2e3be9cc945f9
brinda behal <brindab50@gmail.com>
2019-09-09 06:07:10 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -10,16 +10,25 @@ def consolidate_cart(cart)
        hash[name][:count] = 1
        end 
      end
+
+      hash[name][:count] = 1
+  item_hash = {}
+  cart.each do |item_hash|
+    item_hash.each do |item_hash|
+ if item_hash == name
+      end 
+  
+    end
   end 
   hash
 end
 
-def apply_cupons(cart, cupons)
-  hash = cart 
-  cupons.each do |coupon_hash|
+#def apply_cupons(cart, cupons)
+ #cupons.each do |coupon_hash|
   
   
-end
+#end
+#end 
 
 
 
@@ -289,10 +298,8 @@ end
 
 
 
-
-
-
-
+=======
+>>>>>>> c2a30ffd2df3f9499a5cd481024634ad904d8a81
 def apply_coupons(cart, coupons)
   
 end
