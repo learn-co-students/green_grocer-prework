290726a98c0d9a55687b54d1b18a4d3690af499d
brinda behal <brindab50@gmail.com>
2019-09-10 17:06:35 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -33,8 +33,15 @@ end
 def apply_clearance(cart)
   coupons.each do |coupon|
     item = coupon[:item]
-    if cart[item]
-end
+    if cart[item] && coupon[:item][:count] >= coupon[:num] && !cart["#(item) W/COUPON"]
+      cart["#(item) W/COUPON"] = {price: coupon[:cost] / coupon[:num] , clearance: cart[item][:clearance], count: coupon[:num]}
+      cart[item][:count] -= coupon[:num]
+      elsif coupon[:item][:count] >= coupon[:num] && !cart["#(item) W/COUPON"]
+      cart["#(item) W/COUPON"][:count] += coupon[:num]
+      end
+    end
+  cart
+end 
 
 
 
